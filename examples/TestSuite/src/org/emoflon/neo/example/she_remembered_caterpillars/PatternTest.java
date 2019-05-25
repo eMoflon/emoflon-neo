854cc954a4bf48413a72d34fe63a1692bfbaa3d3
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-05-25 12:26:58 +0200

    Wrote some new tests

--- examples/TestSuite/src/org/emoflon/neo/example/she_remembered_caterpillars/PatternTest.java
+++ examples/TestSuite/src/org/emoflon/neo/example/she_remembered_caterpillars/PatternTest.java
@@ -4,36 +4,43 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import org.emoflon.neo.api.API_Emsl_SheRememberedCaterpillars;
-import org.emoflon.neo.emsl.eMSL.Model;
 import org.emoflon.neo.example.ENeoTest;
-import org.emoflon.neo.neo4j.adapter.NeoPattern;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class PatternTest extends ENeoTest {
 	private API_Emsl_SheRememberedCaterpillars entities = new API_Emsl_SheRememberedCaterpillars(builder);
-	private Model model = entities.getModel_SimpleGame();
 	
 	@BeforeEach
 	private void initDB() {
-		builder.exportEMSLEntityToNeo4j(model);
-		logger.info("-----------------------------\n" + "Database initialised.");
+		initDB(entities.getModel_SimpleGame());
 	}
 	
 	@Test
 	@Disabled("TODO[Jannik] Extend patterns to handle attribute expressions")
 	public void testPatternWithAttributeExpression() {
-		NeoPattern p = entities.getPattern_CanCrossBridge();
-		var matches = p.getMatches();
-		assertThat(matches.size(), is(1));
+		expectSingleMatch(entities.getPattern_CanCrossBridge());
 	}
 	
 	@Test
 	@Disabled("TODO[Jannik] Extend patterns to handle enums")
 	public void testPatternWithEnum() {
-		NeoPattern p = entities.getPattern_EverythingBlue();
-		var matches = p.getMatches();
-		assertThat(matches.size(), is(1));
+		expectSingleMatch(entities.getPattern_EverythingBlue());
+	}
+	
+	@Test
+	public void noStrangeBridge() {
+		expectNoMatch(entities.getPattern_StrangeBridge());
+	}
+	
+	@Test
+	public void inFrontOfBridge() {
+		expectSingleMatch(entities.getPattern_StandingInFrontOfBridge());
+	}
+	
+	@Test
+	public void colouredThingsOnPlatforms() {
+		assertThat(entities.getPattern_ColouredThingOnPlatform().countMatches(), is(2));
 	}
 }
