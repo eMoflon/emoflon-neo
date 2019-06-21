83e840f1a28af14100eee56e0a39362aaf8dba7a
Jannik Hinz <jannikh@mail.uni-paderborn.de>
2019-06-21 16:44:50 +0200

    remove unused if-terms

--- org.emoflon.neo.neo4j.adapter/src/org/emoflon/neo/neo4j/adapter/NeoConstraint.java
+++ org.emoflon.neo.neo4j.adapter/src/org/emoflon/neo/neo4j/adapter/NeoConstraint.java
@@ -24,51 +24,33 @@ public class NeoConstraint implements IConstraint {
 	private NeoCoreBuilder builder;
 
 	private Constraint c;
-	private Collection<NeoNode> nodes;
-	private Collection<NeoPositiveConstraint> positivePattern;
-	private Collection<NeoNegativeConstraint> negativePattern;
-	private Collection<NeoImplication> ifThenPattern;
+	private Collection<NeoPositiveConstraint> positiveConstraint;
+	private Collection<NeoNegativeConstraint> negativeConstraint;
+	private Collection<NeoImplication> implicationConstraint;
 	List<EObject> ref;
 		
 	public NeoConstraint(Constraint c, NeoCoreBuilder builder) {
-		this.nodes = new ArrayList<>();
-		this.positivePattern = new ArrayList<NeoPositiveConstraint>();
-		this.negativePattern = new ArrayList<NeoNegativeConstraint>();
-		this.ifThenPattern = new ArrayList<NeoImplication>();
+		this.positiveConstraint = new ArrayList<NeoPositiveConstraint>();
+		this.negativeConstraint = new ArrayList<NeoNegativeConstraint>();
+		this.implicationConstraint = new ArrayList<NeoImplication>();
 		this.builder = builder;
 		this.c = c;
 		
-		if(c.getBody() instanceof PositiveConstraint) {
-			
-			if(c.getBody().eCrossReferences().get(0) instanceof AtomicPattern) {
-				var ap = (AtomicPattern) c.getBody().eCrossReferences().get(0);
-				positivePattern.add(new NeoPositiveConstraint(ap,builder));
-				
-			} else {
-				throw new UnsupportedOperationException();
-			}
+		if(c.getBody() instanceof PositiveConstraint) {			
+			var ap = (AtomicPattern) c.getBody().eCrossReferences().get(0);
+			positiveConstraint.add(new NeoPositiveConstraint(ap,builder));
 			
 		} else if(c.getBody() instanceof NegativeConstraint) {
+			var ap = (AtomicPattern) c.getBody().eCrossReferences().get(0);
+			negativeConstraint.add(new NeoNegativeConstraint(ap,builder));
 			
-			if(c.getBody().eCrossReferences().get(0) instanceof AtomicPattern) {
-				var ap = (AtomicPattern) c.getBody().eCrossReferences().get(0);
-				negativePattern.add(new NeoNegativeConstraint(ap,builder));
-				
-			} else {
-				throw new UnsupportedOperationException();
-			}
+		} else if(c.getBody() instanceof Implication) {
+			var apIf = (AtomicPattern) c.getBody().eCrossReferences().get(0);
+			var apThen = (AtomicPattern) c.getBody().eCrossReferences().get(1);
+			implicationConstraint.add(new NeoImplication(apIf,apThen,builder));
 			
-		}else if(c.getBody() instanceof Implication) {
-			
-			if(c.getBody().eCrossReferences().get(0) instanceof AtomicPattern && c.getBody().eCrossReferences().get(1) instanceof AtomicPattern) {
-				var apIf = (AtomicPattern) c.getBody().eCrossReferences().get(0);
-				var apThen = (AtomicPattern) c.getBody().eCrossReferences().get(1);
-				ifThenPattern.add(new NeoImplication(apIf,apThen,builder));
-				
-			} else {
-				throw new UnsupportedOperationException();
-			}
-			
+		} else {
+			throw new UnsupportedOperationException(c.getBody().toString());
 		}
 		
 		logger.info(c.getBody().toString());
@@ -78,22 +60,19 @@ public class NeoConstraint implements IConstraint {
 	@Override
 	public boolean isSatisfied() {
 		
-		for(NeoPositiveConstraint ap : positivePattern) {
-			
-			return ap.isSatisfied();
-			
+		for(NeoPositiveConstraint ap : positiveConstraint) {
+			if(!ap.isSatisfied())
+				return false;
 		}
-		for(NeoNegativeConstraint ap : negativePattern) {
-			
-			return ap.isSatisfied();
-			
+		for(NeoNegativeConstraint ap : negativeConstraint) {
+			if(!ap.isSatisfied())
+				return false;
 		}
-		for(NeoImplication ap : ifThenPattern) {
-			
-			return ap.isSatisfied();
-			
+		for(NeoImplication ap : implicationConstraint) {
+			if(!ap.isSatisfied())
+				return false;
 		}
-		return false;
+		return true;
 	}
 
 }
