1c5d0b199a82bd2d63be756b30c1bffaf53492c4
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-09-06 20:55:24 +0200

    Generator should only depend on engine

--- org.emoflon.neo.engine/src/org/emoflon/neo/engine/api/transformation/ITransformation.java
+++
@@ -1,9 +0,0 @@
-package org.emoflon.neo.engine.api.transformation;
-
-public interface ITransformation {
-	void registerObserver(ITransformationObserver observer);
-
-	void setPolicy(ITransformationPolicy policy);
-
-	void run();
-}
--- org.emoflon.neo.engine/src/org/emoflon/neo/engine/api/transformation/ITransformationObserver.java
+++
@@ -1,7 +0,0 @@
-package org.emoflon.neo.engine.api.transformation;
-
-public interface ITransformationObserver {
-	void notifyTransformationStart();
-
-	void notifyTransformationStop();
-}
