6bca8e4887a0904042808e8270e4709fc68fcace
Mario Treiber <mariotreiber@gmail.com>
2019-09-11 13:19:48 +0200

    Added TimedTerminationCondition

---
+++ org.emoflon.neo.generator/src/org/emoflon/neo/engine/modules/TimedTerminationCondition.java
@@ -0,0 +1,25 @@
+package org.emoflon.neo.engine.modules;
+
+import org.emoflon.neo.engine.generator.ITerminationCondition;
+
+public class TimedTerminationCondition implements ITerminationCondition {
+
+	private long startTime;
+	private long maxDuration;
+
+	public TimedTerminationCondition(long pMaxDuration) {
+		maxDuration = pMaxDuration;
+	}
+
+	public void start() {
+		startTime = System.currentTimeMillis();
+	}
+
+	@Override
+	public boolean isReached() {
+		if (System.currentTimeMillis() >= startTime + maxDuration)
+			return true;
+		else
+			return false;
+	}
+}
