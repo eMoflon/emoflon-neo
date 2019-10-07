c6d6d29af08c6194987460b8cd22be752234bb06
Anthony Anjorin <anthony.anjorin@upb.de>
2019-10-07 13:30:17 +0200

    Added stub for GEN with NACs

--- examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_NAC_GEN_Run.java
+++ examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_NAC_GEN_Run.java
@@ -3,8 +3,8 @@ package run;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.emoflon.neo.api.API_Common;
+import org.emoflon.neo.api.API_FacebookToInstagramGrammar_NAC_GEN;
 import org.emoflon.neo.api.API_Transformations;
-import org.emoflon.neo.api.Transformations.API_FacebookToInstagramGrammar_GEN;
 import org.emoflon.neo.engine.generator.Generator;
 import org.emoflon.neo.engine.modules.matchreprocessors.ParanoidNeoReprocessor;
 import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
@@ -14,8 +14,8 @@ import org.emoflon.neo.engine.modules.updatepolicies.RandomSingleMatchUpdatePoli
 import org.emoflon.neo.neo4j.adapter.patterns.NeoMatch;
 import org.emoflon.neo.neo4j.adapter.rules.NeoCoMatch;
 
-public class FacebookToInstagramFASE_GEN_Run {
-	private static final Logger logger = Logger.getLogger(FacebookToInstagramFASE_GEN_Run.class);
+public class FacebookToInstagramFASE_NAC_GEN_Run {
+	private static final Logger logger = Logger.getLogger(FacebookToInstagramFASE_NAC_GEN_Run.class);
 
 	public static void main(String[] pArgs) throws Exception {
 		Logger.getRootLogger().setLevel(Level.INFO);
@@ -33,8 +33,8 @@ public class FacebookToInstagramFASE_GEN_Run {
 					new ParanoidNeoReprocessor(), //
 					new HeartBeatAndReportMonitor());
 
-			var genAPI = new API_FacebookToInstagramGrammar_GEN(builder);			
-			generator.generate(genAPI.getAllRulesForFacebookToInstagramGrammar__GEN());
+			var genAPI = new API_FacebookToInstagramGrammar_NAC_GEN(builder);
+			generator.generate(genAPI.getAllRulesForFacebookToInstagramGrammarNAC__GEN());
 			
 			logger.info("Generation done.");
 		} finally {
