6f1bbc0f61660e0ef0f1ef1c8f1b838938e81220
Anthony Anjorin <anthony.anjorin@upb.de>
2019-10-07 13:25:58 +0200

    Updated stubs

--- examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_CO_Run.java
+++ examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_CO_Run.java
@@ -1,19 +1,17 @@
 package run;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.emoflon.neo.api.API_Common;
-import org.emoflon.neo.api.API_Languages;
 import org.emoflon.neo.api.API_Transformations;
 import org.emoflon.neo.api.Transformations.API_FacebookToInstagramGrammar_CO;
 import org.emoflon.neo.api.Transformations.API_FacebookToInstagramGrammar_GEN;
 import org.emoflon.neo.engine.generator.Generator;
 import org.emoflon.neo.engine.modules.ilp.ILPFactory.SupportedILPSolver;
 import org.emoflon.neo.engine.modules.matchreprocessors.NoOpReprocessor;
-import org.emoflon.neo.engine.modules.monitors.SimpleLoggerMonitor;
+import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
 import org.emoflon.neo.engine.modules.ruleschedulers.AllRulesAllMatchesScheduler;
 import org.emoflon.neo.engine.modules.terminationcondition.OneShotTerminationCondition;
 import org.emoflon.neo.engine.modules.updatepolicies.CheckOnlyOperationalStrategy;
@@ -25,7 +23,7 @@ public class FacebookToInstagramFASE_CO_Run {
 	private static final SupportedILPSolver solver = SupportedILPSolver.Gurobi;
 
 	public static void main(String[] pArgs) throws Exception {
-		Logger.getRootLogger().setLevel(Level.DEBUG);
+		Logger.getRootLogger().setLevel(Level.INFO);
 
 		var builder = API_Common.createBuilder();
 
@@ -38,24 +36,23 @@ public class FacebookToInstagramFASE_CO_Run {
 					api.getConstraint_NoDoubleFriendship());
 
 			var genAPI = new API_FacebookToInstagramGrammar_GEN(builder);
-			var checkOnly = new CheckOnlyOperationalStrategy(genAPI.getAllRules(), negativeConstraints);
+			var checkOnly = new CheckOnlyOperationalStrategy(genAPI.getAllRulesForFacebookToInstagramGrammar__GEN(), negativeConstraints);
 
 			Generator<NeoMatch, NeoCoMatch> generator = new Generator<NeoMatch, NeoCoMatch>(//
 					new OneShotTerminationCondition(), //
 					new AllRulesAllMatchesScheduler(), //
 					checkOnly, //
 					new NoOpReprocessor(), //
-					new SimpleLoggerMonitor());
+					new HeartBeatAndReportMonitor());
 
 			var coAPI = new API_FacebookToInstagramGrammar_CO(builder);
-			generator.generate(coAPI.getAllRules());
+			generator.generate(coAPI.getAllRulesForFacebookToInstagramGrammar__CO());
 
+			logger.info("Invoking ILP solver...");
 			if (checkOnly.isConsistent(solver))
 				logger.info("Your triple is consistent!");
 			else {
 				logger.info("Your triple is inconsistent!");
-				
-				logger.info("Now trying to determine inconsistent elements (this might take much longer):");
 				var inconsistentElements = checkOnly.determineInconsistentElements(solver);
 				logger.info(inconsistentElements.get().size() + " elements of your triple are inconsistent!");
 			}
--- examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_GEN_Run.java
+++ examples/FacebookToInstagramFASE/src/run/FacebookToInstagramFASE_GEN_Run.java
@@ -4,10 +4,10 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.emoflon.neo.api.API_Common;
 import org.emoflon.neo.api.API_Transformations;
-import org.emoflon.neo.api.Transformations.API_FacebookToInstagramGrammar_GEN_NAC;
+import org.emoflon.neo.api.Transformations.API_FacebookToInstagramGrammar_GEN;
 import org.emoflon.neo.engine.generator.Generator;
 import org.emoflon.neo.engine.modules.matchreprocessors.ParanoidNeoReprocessor;
-import org.emoflon.neo.engine.modules.monitors.SimpleLoggerMonitor;
+import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
 import org.emoflon.neo.engine.modules.ruleschedulers.AllRulesAllMatchesScheduler;
 import org.emoflon.neo.engine.modules.terminationcondition.TimedTerminationCondition;
 import org.emoflon.neo.engine.modules.updatepolicies.RandomSingleMatchUpdatePolicy;
@@ -18,7 +18,7 @@ public class FacebookToInstagramFASE_GEN_Run {
 	private static final Logger logger = Logger.getLogger(FacebookToInstagramFASE_GEN_Run.class);
 
 	public static void main(String[] pArgs) throws Exception {
-		Logger.getRootLogger().setLevel(Level.DEBUG);
+		Logger.getRootLogger().setLevel(Level.INFO);
 		
 		var builder = API_Common.createBuilder();
 
@@ -27,16 +27,17 @@ public class FacebookToInstagramFASE_GEN_Run {
 			api.exportMetamodelsForFacebookToInstagramGrammar();
 
 			Generator<NeoMatch, NeoCoMatch> generator = new Generator<NeoMatch, NeoCoMatch>(//
-					new TimedTerminationCondition(30000), 
+					new TimedTerminationCondition(3000), 
 					new AllRulesAllMatchesScheduler(), //
 					new RandomSingleMatchUpdatePolicy(), //
 					new ParanoidNeoReprocessor(), //
-					new SimpleLoggerMonitor());
+					new HeartBeatAndReportMonitor());
 
-			//var genAPI = new API_FacebookToInstagramGrammar_GEN(builder);
-			var genAPI = new API_FacebookToInstagramGrammar_GEN_NAC(builder);
+			var genAPI = new API_FacebookToInstagramGrammar_GEN(builder);
+			//var genAPI = new API_FacebookToInstagramGrammar_NAC_GEN(builder);
 			
-			generator.generate(genAPI.getAllRules());
+			generator.generate(genAPI.getAllRulesForFacebookToInstagramGrammar__GEN());
+			//generator.generate(genAPI.getAllRulesForFacebookToInstagramGrammarNAC__GEN());
 			
 			logger.info("Generation done.");
 		} finally {
