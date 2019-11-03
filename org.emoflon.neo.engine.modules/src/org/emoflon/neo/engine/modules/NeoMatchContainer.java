4b1e0b0d0282fa86f898cb2598f74107dcc16075
Mario Treiber <mtr@mail.upb.de>
2019-11-03 22:02:19 +0100

    Implemented rule masking in NeoMatchContainer

--- org.emoflon.neo.engine.modules/src/org/emoflon/neo/engine/modules/NeoMatchContainer.java
+++ org.emoflon.neo.engine.modules/src/org/emoflon/neo/engine/modules/NeoMatchContainer.java
@@ -1,19 +1,52 @@
 package org.emoflon.neo.engine.modules;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+import org.emoflon.neo.emsl.eMSL.ConstraintBody;
+import org.emoflon.neo.emsl.eMSL.ModelNodeBlock;
+import org.emoflon.neo.emsl.eMSL.ModelPropertyStatement;
+import org.emoflon.neo.emsl.eMSL.Parameter;
+import org.emoflon.neo.emsl.util.EMSLUtil;
 import org.emoflon.neo.engine.api.rules.IRule;
 import org.emoflon.neo.engine.generator.MatchContainer;
+import org.emoflon.neo.neo4j.adapter.patterns.AttributeMask;
 import org.emoflon.neo.neo4j.adapter.patterns.NeoMatch;
 import org.emoflon.neo.neo4j.adapter.rules.NeoCoMatch;
 import org.emoflon.neo.neo4j.adapter.rules.NeoRule;
+import org.emoflon.neo.neo4j.adapter.rules.NeoRuleFactory;
 
 public class NeoMatchContainer extends MatchContainer<NeoMatch, NeoCoMatch> {
 
 	public NeoMatchContainer(Collection<? extends IRule<NeoMatch, NeoCoMatch>> allRules) {
-		super(allRules);
+		super(maskParameters(allRules));
+	}
+
+	private static Collection<NeoRule> maskParameters(Collection<? extends IRule<NeoMatch, NeoCoMatch>> allRules) {
+		Collection<NeoRule> maskedRules = new HashSet<>();
+
+		for (IRule<NeoMatch, NeoCoMatch> iRule : allRules) {
+			if (!(iRule instanceof NeoRule))
+				throw new IllegalStateException("Unexpected type of rule: " + iRule.getClass());
+			NeoRule rule = (NeoRule) iRule;
+			Collection<ModelNodeBlock> nodeBlocks = new HashSet<>();
+			nodeBlocks.addAll(rule.getEMSLRule().getNodeBlocks());
+			EMSLUtil.iterateConstraintPatterns((ConstraintBody) rule.getEMSLRule().getCondition(),
+					pattern -> nodeBlocks.addAll(pattern.getNodeBlocks()));
+			AttributeMask mask = new AttributeMask();
+
+			for (ModelNodeBlock nodeBlock : nodeBlocks)
+				for (ModelPropertyStatement prop : nodeBlock.getProperties())
+					if (prop.getValue() instanceof Parameter)
+						mask.maskAttribute(nodeBlock.getName() + "." + prop.getType().getName(),
+								"$" + ((Parameter) prop.getValue()).getName());
+
+			maskedRules.add(NeoRuleFactory.copyNeoRuleWithNewMask(rule, mask));
+		}
+
+		return maskedRules;
 	}
 
 	@Override
