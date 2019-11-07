9eef752986f3696b48a2f34494bf7ef3c8236cc0
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-11-07 19:56:19 +0100

    Added stubs for SokobanImportExport TGG

---
+++ examples/SokobanLanguage/src/run/SokobanImportExport_CO_Run.java
@@ -0,0 +1,70 @@
+package run;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.emoflon.neo.api.API_Common;
+import org.emoflon.neo.api.rules.SokobanTGGs.API_SokobanImportExport_CO;
+import org.emoflon.neo.api.rules.SokobanTGGs.API_SokobanImportExport_GEN;
+import org.emoflon.neo.cypher.models.NeoCoreBuilder;
+import org.emoflon.neo.engine.api.constraints.IConstraint;
+import org.emoflon.neo.engine.modules.NeoGenerator;
+import org.emoflon.neo.engine.modules.ilp.ILPFactory.SupportedILPSolver;
+import org.emoflon.neo.engine.modules.matchreprocessors.NoOpReprocessor;
+import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
+import org.emoflon.neo.engine.modules.ruleschedulers.AllRulesAllMatchesScheduler;
+import org.emoflon.neo.engine.modules.terminationcondition.OneShotTerminationCondition;
+import org.emoflon.neo.engine.modules.updatepolicies.CheckOnlyOperationalStrategy;
+import org.emoflon.neo.engine.modules.valueGenerators.LoremIpsumStringValueGenerator;
+import org.emoflon.neo.engine.modules.valueGenerators.ModelNameValueGenerator;
+
+public class SokobanImportExport_CO_Run {
+	private static final Logger logger = Logger.getLogger(SokobanImportExport_CO_Run.class);
+	private static final SupportedILPSolver solver = SupportedILPSolver.Sat4J;
+
+	public static void main(String[] pArgs) throws Exception {
+		Logger.getRootLogger().setLevel(Level.INFO);
+		var app = new SokobanImportExport_CO_Run();
+		app.runCheckOnly();
+	}
+
+	public boolean runCheckOnly() throws Exception {
+		try (var builder = API_Common.createBuilder()) {
+			var genAPI = new API_SokobanImportExport_GEN(builder);
+			var checkOnly = new CheckOnlyOperationalStrategy(genAPI.getAllRulesForSokobanImportExport__GEN(),
+					getNegativeConstraints(builder));
+
+			var coAPI = new API_SokobanImportExport_CO(builder);
+			var generator = new NeoGenerator(//
+					coAPI.getAllRulesForSokobanImportExport__CO(), //
+					new OneShotTerminationCondition(), //
+					new AllRulesAllMatchesScheduler(), //
+					checkOnly, //
+					new NoOpReprocessor(), //
+					new HeartBeatAndReportMonitor(), //
+					new ModelNameValueGenerator("TheSource", "TheTarget"), //
+					List.of(new LoremIpsumStringValueGenerator()));
+
+			logger.info("Start check only...");
+			generator.generate();
+
+			if (checkOnly.isConsistent(solver)) {
+				logger.info("Your triple is consistent!");
+				return true;
+			} else {
+				logger.info("Your triple is inconsistent!");
+				var inconsistentElements = checkOnly.determineInconsistentElements(solver);
+				logger.info(inconsistentElements.get().size() + " elements of your triple are inconsistent!");
+				return false;
+			}
+		}
+	}
+
+	protected Collection<IConstraint> getNegativeConstraints(NeoCoreBuilder builder) {
+		return List.of(//
+
+		);
+	}
+}
---
+++ examples/SokobanLanguage/src/run/SokobanImportExport_GEN_Run.java
@@ -0,0 +1,67 @@
+package run;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.emoflon.neo.api.API_Common;
+import org.emoflon.neo.api.rules.API_SokobanTGGs;
+import org.emoflon.neo.api.rules.SokobanTGGs.API_SokobanImportExport_GEN;
+import org.emoflon.neo.cypher.rules.NeoRule;
+import org.emoflon.neo.emsl.util.FlattenerException;
+import org.emoflon.neo.engine.generator.INodeSampler;
+import org.emoflon.neo.engine.modules.NeoGenerator;
+import org.emoflon.neo.engine.modules.matchreprocessors.ParanoidNeoReprocessor;
+import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
+import org.emoflon.neo.engine.modules.ruleschedulers.TwoPhaseRuleSchedulerForGEN;
+import org.emoflon.neo.engine.modules.terminationcondition.CompositeTerminationConditionForGEN;
+import org.emoflon.neo.engine.modules.terminationcondition.MaximalRuleApplicationsTerminationCondition;
+import org.emoflon.neo.engine.modules.updatepolicies.TwoPhaseUpdatePolicyForGEN;
+import org.emoflon.neo.engine.modules.valueGenerators.LoremIpsumStringValueGenerator;
+import org.emoflon.neo.engine.modules.valueGenerators.ModelNameValueGenerator;
+
+public class SokobanImportExport_GEN_Run {
+	private static final Logger logger = Logger.getLogger(SokobanImportExport_GEN_Run.class);
+
+	public static void main(String[] pArgs) throws Exception {
+		Logger.getRootLogger().setLevel(Level.INFO);
+		var app = new SokobanImportExport_GEN_Run();
+		app.runGenerator();
+	}
+
+	public void runGenerator() throws FlattenerException, Exception {
+		try (var builder = API_Common.createBuilder()) {
+			var api = new API_SokobanTGGs(builder);
+			api.exportMetamodelsForSokobanImportExport();
+
+			var genAPI = new API_SokobanImportExport_GEN(builder);
+			var generator = createGenerator(genAPI);
+
+			logger.info("Start model generation...");
+			generator.generate();
+			logger.info("Generation done.");
+		}
+	}
+
+	protected NeoGenerator createGenerator(API_SokobanImportExport_GEN genAPI) {
+		Collection<NeoRule> allRules = genAPI.getAllRulesForSokobanImportExport__GEN();
+
+		var maxRuleApps = new MaximalRuleApplicationsTerminationCondition(allRules, -1);
+
+		INodeSampler sampler = (String type, String ruleName, String nodeName) -> {
+			return INodeSampler.EMPTY;
+		};
+
+		return new NeoGenerator(//
+				allRules, //
+				new CompositeTerminationConditionForGEN(5, TimeUnit.MINUTES, maxRuleApps), //
+				new TwoPhaseRuleSchedulerForGEN(sampler), //
+				new TwoPhaseUpdatePolicyForGEN(maxRuleApps), //
+				new ParanoidNeoReprocessor(), //
+				new HeartBeatAndReportMonitor(), //
+				new ModelNameValueGenerator("TheSource", "TheTarget"), //
+				List.of(new LoremIpsumStringValueGenerator()));
+	}
+}
