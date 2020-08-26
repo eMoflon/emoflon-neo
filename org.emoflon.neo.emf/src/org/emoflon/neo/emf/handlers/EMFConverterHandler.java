1ac9a0eabf13b0b24f9fce71fb6baa75cee14599
Anthony Anjorin <anthony.anjorin@iav.de>
2020-08-26 17:28:30 +0200

    Importer now uses resource set from reflective ecore editor

--- org.emoflon.neo.emf/src/org/emoflon/neo/emf/handlers/EMFConverterHandler.java
+++ org.emoflon.neo.emf/src/org/emoflon/neo/emf/handlers/EMFConverterHandler.java
@@ -3,9 +3,7 @@ package org.emoflon.neo.emf.handlers;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
@@ -13,12 +11,16 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.SaveAsDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.emoflon.neo.emf.EMFImporter;
 
@@ -29,62 +31,41 @@ public class EMFConverterHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		var selection = HandlerUtil.getCurrentStructuredSelection(event);
-		List<?> selected = selection.toList();
-
-		if (!selected.stream().allMatch(this::isOfRelevantType)) {
-			logger.info("Only .ecore and .xmi files can be converted to EMSL.");
-		} else {
-			var files = selected.stream().map(IFile.class::cast).collect(Collectors.toList());
-			try {
-				logger.info("Converting " + files + " to EMSL...");
-				var file = files.get(0);
-				var resourceSet = loadFilesIntoResourceSet(files);
-				var mslContent = new EMFImporter().generateEMSLModel(resourceSet);
-				var path = file.getProjectRelativePath().removeFileExtension().addFileExtension("msl");
-				var mslFile = file.getProject().getFile(path);
-				try (var is = createInputStream(mslContent)) {
-					mslFile.create(is, true, new NullProgressMonitor());
-				} catch (CoreException | IOException e) {
-					throw e;
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+		var shell = HandlerUtil.getActiveEditor(event).getSite().getShell();
+		
+		var resourceSet = extractResourceSet(selection);
+		resourceSet.ifPresent(rs -> {
+			EcoreUtil.resolveAll(rs);
+			var mslContent = new EMFImporter().generateEMSLModels(rs);
+			var mslFile = chooseFileToCreate(shell);
+			try (var is = createInputStream(mslContent)) {
+				mslFile.create(is, true, new NullProgressMonitor());
+			} catch (CoreException | IOException e) {
 				logger.error("Unable to perform conversion to EMSL: " + e);
 			}
-		}
+		});
 
 		return null;
 	}
 
-	private boolean isOfRelevantType(Object file) {
-		if (file instanceof IFile) {
-			var ifile = (IFile) file;
-			return ifile.getFileExtension().equals("xmi") || ifile.getFileExtension().equals("ecore");
-		}
+	private IFile chooseFileToCreate(Shell parentShell) {
+		var saveAsDialog = new SaveAsDialog(parentShell);
+		saveAsDialog.open();
+		var path = saveAsDialog.getResult();
+		return ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+	}
 
-		return false;
+	private Optional<ResourceSet> extractResourceSet(IStructuredSelection selection) {
+		var someObject = selection.getFirstElement();
+		if(someObject instanceof EObject) {
+			return Optional.of(((EObject) someObject).eResource().getResourceSet());
+		} else if(someObject instanceof Resource) {
+			return Optional.of(((Resource) someObject).getResourceSet());
+		} else
+			return Optional.empty();
 	}
 
 	private InputStream createInputStream(String mslContent) {
 		return IOUtils.toInputStream(mslContent, Charset.defaultCharset());
-	}
-
-	private ResourceSet loadFilesIntoResourceSet(List<IFile> files) throws IOException {
-		var rs = new ResourceSetImpl();
-		Collections.sort(files, (x,y) -> x.getFileExtension().compareTo(y.getFileExtension()));
-		for (var file : files) {
-			var r = rs.createResource(URI.createFileURI(file.getLocation().toString()));
-			r.load(null);
-			if (file.getFileExtension().equals("ecore")) {
-				var pack = r.getContents().get(0);
-				if (pack instanceof EPackage) {
-					var uri = ((EPackage) pack).getNsURI();
-					r.setURI(URI.createURI(uri));
-					rs.getPackageRegistry().put(uri, pack);
-				}
-			}
-		}
-
-		return rs;
 	}
 }
