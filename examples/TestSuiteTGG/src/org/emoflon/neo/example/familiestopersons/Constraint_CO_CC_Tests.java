9882d4f85e04efb45359fad121e6972a67919e41
Anthony Anjorin <tonyanjorin@googlemail.com>
2022-10-01 15:56:40 +0200

    Make sure tests use the solver for tests and not for the generated app

--- examples/TestSuiteTGG/src/org/emoflon/neo/example/familiestopersons/Constraint_CO_CC_Tests.java
+++ examples/TestSuiteTGG/src/org/emoflon/neo/example/familiestopersons/Constraint_CO_CC_Tests.java
@@ -7,74 +7,80 @@ import org.junit.jupiter.api.Test;
 import org.emoflon.neo.api.familiestopersons_constrained.run.FamiliesToPersons_Constrained_CC_Run;
 import org.emoflon.neo.api.familiestopersons_constrained.run.FamiliesToPersons_Constrained_CO_Run;
 
-public class Constraint_CO_CC_Tests extends ENeoTest{
-	
-	private API_FamiliesToPersonsTriplesForConstraintTesting api = new API_FamiliesToPersonsTriplesForConstraintTesting(builder);
-	
-	    //co-cc for consistent triple1
-		@Test
-		public void testConsistentTriple1_CO() throws Exception {
-			exportTriple(api.getModel_ConsistentSource1(), //
-					api.getModel_ConsistentTarget1(), //
-					api.getRule_ConsistentTriple1().rule());
-			testForConsistency(new FamiliesToPersons_Constrained_CO_Run("ConsistentSource1", "ConsistentTarget1").runCheckOnly(), 3);
-		}
-		
-		@Test
-		public void testConsistentTriple1_CC() throws Exception {
-			exportTriple(api.getModel_ConsistentSource1(), //
-					api.getModel_ConsistentTarget1());
-			testForConsistency(new FamiliesToPersons_Constrained_CC_Run("ConsistentSource1", "ConsistentTarget1").runCorrCreation(),
-					3);
-		}
-		
-		//co for inconsistent triple1
-		@Test
-		public void testInConsistentTriple1_CO() throws Exception {
-			exportTriple(api.getModel_InConsistentSource1(), //
-					api.getModel_InConsistentTarget1(), //
-					api.getRule_InConsistentTriple1().rule());
-			testForInconsistency(new FamiliesToPersons_Constrained_CO_Run("InConsistentSource1", "InConsistentTarget1").runCheckOnly(),
-					3, 7);
-		}
-		
-		//co-cc for consistent triple2
-				@Test
-				public void testConsistentTriple2_CO() throws Exception {
-					exportTriple(api.getModel_ConsistentSource2(), //
-							api.getModel_ConsistentTarget2(), //
-							api.getRule_ConsistentTriple2().rule());
-					testForConsistency(new FamiliesToPersons_Constrained_CO_Run("ConsistentSource2", "ConsistentTarget2").runCheckOnly(), 20);
-				}
-						
-				@Test
-				public void testConsistentTriple2_CC() throws Exception {
-					exportTriple(api.getModel_ConsistentSource2(), //
-							api.getModel_ConsistentTarget2());
-					testForConsistency(new FamiliesToPersons_Constrained_CC_Run("ConsistentSource2", "ConsistentTarget2").runCorrCreation(),
-							20);
-				}
-		
-		//co for inconsistent triple2
-		@Test
-		public void testInConsistentTriple2_CO() throws Exception {
-			exportTriple(api.getModel_InConsistentSource2(), //
-					api.getModel_InConsistentTarget2(), //
-					api.getRule_InConsistentTriple2().rule());
-			testForInconsistency(new FamiliesToPersons_Constrained_CO_Run("InConsistentSource2", "InConsistentTarget2").runCheckOnly(),
-					3, 12);
-		}
-				
-		
-				
-		//co for inconsistent triple3
-		@Test
-		public void testInConsistentTriple3_CO() throws Exception {
-			exportTriple(api.getModel_InConsistentSource3(), //
-					api.getModel_InConsistentTarget3(), //
-					api.getRule_InConsistentTriple3().rule());
-			testForInconsistency(new FamiliesToPersons_Constrained_CO_Run("InConsistentSource3", "InConsistentTarget3").runCheckOnly(),
-					3, 12);
-		}
+public class Constraint_CO_CC_Tests extends ENeoTest {
+
+	private API_FamiliesToPersonsTriplesForConstraintTesting api = new API_FamiliesToPersonsTriplesForConstraintTesting(
+			builder);
+
+	// co-cc for consistent triple1
+	@Test
+	public void testConsistentTriple1_CO() throws Exception {
+		exportTriple(api.getModel_ConsistentSource1(), //
+				api.getModel_ConsistentTarget1(), //
+				api.getRule_ConsistentTriple1().rule());
+		testForConsistency(new FamiliesToPersons_Constrained_CO_Run("ConsistentSource1", "ConsistentTarget1", solver)
+				.runCheckOnly(), 3);
+	}
+
+	@Test
+	public void testConsistentTriple1_CC() throws Exception {
+		exportTriple(api.getModel_ConsistentSource1(), //
+				api.getModel_ConsistentTarget1());
+		testForConsistency(new FamiliesToPersons_Constrained_CC_Run("ConsistentSource1", "ConsistentTarget1", solver)
+				.runCorrCreation(), 3);
+	}
+
+	// co for inconsistent triple1
+	@Test
+	public void testInConsistentTriple1_CO() throws Exception {
+		exportTriple(api.getModel_InConsistentSource1(), //
+				api.getModel_InConsistentTarget1(), //
+				api.getRule_InConsistentTriple1().rule());
+		testForInconsistency(
+				new FamiliesToPersons_Constrained_CO_Run("InConsistentSource1", "InConsistentTarget1", solver)
+						.runCheckOnly(),
+				3, 7);
+	}
+
+	// co-cc for consistent triple2
+	@Test
+	public void testConsistentTriple2_CO() throws Exception {
+		exportTriple(api.getModel_ConsistentSource2(), //
+				api.getModel_ConsistentTarget2(), //
+				api.getRule_ConsistentTriple2().rule());
+		testForConsistency(new FamiliesToPersons_Constrained_CO_Run("ConsistentSource2", "ConsistentTarget2", solver)
+				.runCheckOnly(), 20);
+	}
+
+	@Test
+	public void testConsistentTriple2_CC() throws Exception {
+		exportTriple(api.getModel_ConsistentSource2(), //
+				api.getModel_ConsistentTarget2());
+		testForConsistency(new FamiliesToPersons_Constrained_CC_Run("ConsistentSource2", "ConsistentTarget2", solver)
+				.runCorrCreation(), 20);
+	}
+
+	// co for inconsistent triple2
+	@Test
+	public void testInConsistentTriple2_CO() throws Exception {
+		exportTriple(api.getModel_InConsistentSource2(), //
+				api.getModel_InConsistentTarget2(), //
+				api.getRule_InConsistentTriple2().rule());
+		testForInconsistency(
+				new FamiliesToPersons_Constrained_CO_Run("InConsistentSource2", "InConsistentTarget2", solver)
+						.runCheckOnly(),
+				3, 12);
+	}
+
+	// co for inconsistent triple3
+	@Test
+	public void testInConsistentTriple3_CO() throws Exception {
+		exportTriple(api.getModel_InConsistentSource3(), //
+				api.getModel_InConsistentTarget3(), //
+				api.getRule_InConsistentTriple3().rule());
+		testForInconsistency(
+				new FamiliesToPersons_Constrained_CO_Run("InConsistentSource3", "InConsistentTarget3", solver).runCheckOnly(),
+				3, 12);
+	}
 
 }
