5dc11f465e6c499e689781525dc2787bead3e985
Anthony Anjorin <tonyanjorin@googlemail.com>
2022-10-09 20:55:36 +0200

    Added context

--- examples/Liaison/src/liason/app/MyMI.java
+++ examples/Liaison/src/liason/app/MyMI.java
@@ -3,12 +3,31 @@ package liason.app;
 import static org.emoflon.neo.api.liaison.run.RequirementsCoverage_GEN_Run.SRC_MODEL_NAME;
 import static org.emoflon.neo.api.liaison.run.RequirementsCoverage_GEN_Run.TRG_MODEL_NAME;
 
+import java.util.List;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.emoflon.neo.api.liaison.API_Common;
 import org.emoflon.neo.api.liaison.API_Delta;
+import org.emoflon.neo.api.liaison.API_ReqArchTraceability;
 import org.emoflon.neo.api.liaison.run.RequirementsCoverage_MI_Run;
+import org.emoflon.neo.api.liaison.tgg.API_RequirementsCoverage_GEN;
+import org.emoflon.neo.api.liaison.tgg.API_RequirementsCoverage_MI;
+import org.emoflon.neo.cypher.models.NeoCoreBuilder;
+import org.emoflon.neo.cypher.patterns.NeoMatch;
+import org.emoflon.neo.cypher.rules.NeoCoMatch;
+import org.emoflon.neo.engine.generator.MatchContainer;
+import org.emoflon.neo.engine.modules.NeoGenerator;
+import org.emoflon.neo.engine.modules.analysis.TripleRuleAnalyser;
 import org.emoflon.neo.engine.modules.ilp.ILPFactory.SupportedILPSolver;
+import org.emoflon.neo.engine.modules.matchreprocessors.MIReprocessor;
+import org.emoflon.neo.engine.modules.monitors.HeartBeatAndReportMonitor;
+import org.emoflon.neo.engine.modules.ruleschedulers.MIRuleScheduler;
+import org.emoflon.neo.engine.modules.startup.PrepareContextDeltaAttributes;
+import org.emoflon.neo.engine.modules.terminationcondition.NoMoreMatchesTerminationCondition;
+import org.emoflon.neo.engine.modules.updatepolicies.ModelIntegrationOperationalStrategy;
+import org.emoflon.neo.engine.modules.valueGenerators.LoremIpsumStringValueGenerator;
+import org.emoflon.neo.engine.modules.valueGenerators.ModelNameValueGenerator;
 
 public class MyMI extends RequirementsCoverage_MI_Run {
 	
@@ -29,5 +48,35 @@ public class MyMI extends RequirementsCoverage_MI_Run {
 			
 			app.run();
 		}
+	}
+	
+	public NeoGenerator createGenerator(NeoCoreBuilder builder) {
+		var api = new API_ReqArchTraceability(builder);
+		var genAPI = new API_RequirementsCoverage_GEN(builder);
+		var miAPI = new API_RequirementsCoverage_MI(builder);
+		var genRules = genAPI.getAllRulesForRequirementsCoverage_GEN();
+		var analyser = new TripleRuleAnalyser(new API_ReqArchTraceability(builder).getTripleRulesOfRequirementsCoverage());
+				
+		modelIntegration = new ModelIntegrationOperationalStrategy(//
+				solver, //
+				builder, //
+				genRules, //
+				miAPI.getAllRulesForRequirementsCoverage_MI(), //
+				api.getConstraintsOfRequirementsCoverage(), //
+				srcModelName, //
+				trgModelName//
+		);
+		
+		return new NeoGenerator(//
+				miAPI.getAllRulesForRequirementsCoverage_MI(), //
+				new PrepareContextDeltaAttributes(builder, srcModelName, trgModelName), //
+				new NoMoreMatchesTerminationCondition(), //
+				new MIRuleScheduler(analyser), //
+				modelIntegration, //
+				new MIReprocessor(analyser), //
+				modelIntegration, //
+				new HeartBeatAndReportMonitor(), //
+				new ModelNameValueGenerator(srcModelName, trgModelName), //
+				List.of(new LoremIpsumStringValueGenerator()));
 	}
 }
