dae1b74d82c6a1d82de43e9ead864a43195bea03
NilsWeidmann <nilsweid@mail.upb.de>
2022-10-10 09:10:14 +0200

    Add preferences for MI

--- org.emoflon.neo.emf/src-gen/org/emoflon/neo/api/org/emoflon/neo/emf/API_Common.java
+++ org.emoflon.neo.emf/src-gen/org/emoflon/neo/api/org/emoflon/neo/emf/API_Common.java
@@ -7,10 +7,10 @@ import org.emoflon.neo.cypher.models.*;
 public class API_Common {
 	// Default values (might have to be changed)
 	public static final String PLATFORM_PLUGIN_URI = "/C:/Program Files/Eclipse/eclipse-for-java-and-dsl-developers/plugins/";
-	public static final String NEOCORE_URI_INSTALLED = "/C:/Program Files/Eclipse/eclipse-for-java-and-dsl-developers/plugins/org.emoflon.neo.neocore_1.0.0.202103291557/";
+	public static final String NEOCORE_URI_INSTALLED = "/C:/Program Files/Eclipse/eclipse-for-java-and-dsl-developers/plugins/org.emoflon.neo.neocore_1.0.0.202210011604/";
 	public static final String PLATFORM_RESOURCE_URI = "../";
 
 	public static NeoCoreBuilder createBuilder() {
-		return new NeoCoreBuilder("bolt://localhost:7687", "neo4j", "test");
+		return new NeoCoreBuilder("bolt://localhost:7687", "neo4j", "test", -5.0, 5.0, -1.0);
 	}
 }
